3420a74aefb6af0d79a122e653a34e21766f1cda
sparkEEdev <sparkeeftw@gmail.com>
2022-09-28 14:02:42 +0200

    add files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# battle-sim-laravel
+# Battle-Sim-Laravel
+Sample laravel app that scrapes and stores food facts.
+
+### Installation steps;
+
+1. Run `composer install`.
+2. Run `cp .env.example .env` and update it.
+4. Run `php artisan jwt:secret`.
+3. Run `php artisan migrate`.
+5. (Local) Run `php artisan db:seed`.
---
+++ app/Http/Controllers/Api/v1/ArmyController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Models\Army;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+class ArmyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Army  $army
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Army $army)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Army  $army
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Army $army)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Army  $army
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Army $army)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Army  $army
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Army $army)
+    {
+        //
+    }
+}
